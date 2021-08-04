c4eca65e1b48525b1a77e6024dbc0de35a77cdf1
Alexis Chenneviere <alexis.chenneviere@cea.fr>
2021-08-04 09:15:33 +0200

    - remove print functions

--- src/pygdatax/icons.py
+++ src/pygdatax/icons.py
@@ -7,5 +7,4 @@ def getQIcon(name):
     ans = None
     path = os.path.abspath(os.path.dirname(pygdatax.resources.__file__))
     ans = os.path.join(path, name)
-    print(ans)
     return qt.QIcon(ans)
